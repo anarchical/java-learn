d21b91e966394bcf44efaed7cc65a5c82785d020
yeyaqiao <yyq9202021895@163.com>
2021-01-20 15:12:55 +0800

    add test

---
+++ 21-concurrent/src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @Author : YeYaqiao
+ * @Date : 2021-01-20 15:12:27
+ * @E-mail : yeyaqiao@qianxin.com
+ * @Description : TODO
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("fuck");
+    }
+}
