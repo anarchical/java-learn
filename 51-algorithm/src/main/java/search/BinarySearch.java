69c9a1032d7f4813694bd32d06ed71e5078d0d04
YeYaqiao <yyq920201895@163.com>
2021-04-17 01:13:58 +0800

    add BinarySearch

--- 51-algorithm/src/main/java/search/BinarySearch.java
+++ 51-algorithm/src/main/java/search/BinarySearch.java
@@ -6,23 +6,26 @@ import java.util.Arrays;
  * @author YeYaqiao
  * 二分查找（适用于排好序的顺序存储）
  * 时间复杂度：O(logN)
+ * 空间复杂度：O(1)
  */
 public class BinarySearch {
 
     public static boolean search(int[] array, int target) {
         Arrays.sort(array);
-        int start = 0;
-        int end = array.length - 1;
-        int middle = end + (start - end) / 2;
-        while (start < end) {
 
-            if (array[middle] == target) {
+        int right = 0;
+        int left = array.length - 1;
+        int middle;
+
+        while (right <= left) {
+            middle = right + (left - right) / 2;
+
+            if (array[middle] == target)
                 return true;
-            }else if(array[middle]<target){
-                end=middle;
-            }else if(array[middle]>target){
-                start=middle;
-            }
+            if (array[middle] < target)
+                right = middle + 1;
+            if (array[middle] > target)
+                left = middle - 1;
         }
 
         return false;
@@ -31,7 +34,7 @@ public class BinarySearch {
     public static void main(String[] args) {
 
         int[] array = new int[]{6, 5, 3, 2, 5, 1, 9, 8, 7, 0};
-        search(array, 1);
+        System.out.println(search(array, 8));
     }
 
 }
