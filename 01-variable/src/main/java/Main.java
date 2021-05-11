a75d962370f3ca623a8b8592c3ad59abff12d5d7
yeyaqiao <yyq9202021895@163.com>
2021-05-11 16:03:58 +0800

    add something

---
+++ 01-variable/src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author YeYaqiao
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        float a = 10.1f;
+        int b = (int) a;
+        System.out.println(b);
+        System.out.println("fuck");
+    }
+}
