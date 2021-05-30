ecd7c67a11beb8e7b97c7d95f822f4d6aa113902
yyq <yyq920201895@163.com>
2021-05-31 00:11:15 +0800

    add concurrent test

---
+++ 09-concurrent/src/main/java/test/Clock.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author YeYaqiao
+ */
+public class Clock implements Runnable {
+
+    private final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    @Override
+    public void run() {
+
+        while (true) {
+
+            Date date = new Date();
+            System.out.println(simpleDateFormat.format(date));
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Clock clock = new Clock();
+        Thread thread = new Thread(clock);
+
+        thread.start();
+    }
+}
