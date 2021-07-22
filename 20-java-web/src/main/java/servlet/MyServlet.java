4d0ccc8539e33f90e11081bfffb83d424a911d2d
yeyaqiao <yyq9202021895@163.com>
2021-07-22 22:15:59 +0800

    add tomcat

--- 20-java-web/src/main/java/servlet/MyServlet.java
+++ 20-java-web/src/main/java/servlet/MyServlet.java
@@ -1,7 +1,37 @@
 package servlet;
 
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
 /**
  * @author YeYaqiao
  */
-public class MyServlet {
+@WebServlet("/myServlet")
+public class MyServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
